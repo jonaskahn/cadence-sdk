a86b9fdddb90c653c8946c1d7137e20ed8ee6146
Jonas Kahn <tuyendev@gmail.com>
2025-09-04 00:24:48 +0700

    Fix debug log level

--- src/cadence_sdk/__init__.py
+++ src/cadence_sdk/__init__.py
@@ -17,7 +17,7 @@ from cadence_sdk.base.plugin import BasePlugin
 from cadence_sdk.registry.plugin_registry import PluginRegistry, discover_plugins, get_plugin_registry, register_plugin
 from cadence_sdk.tools.decorators import tool
 
-__version__ = "1.0.8"
+__version__ = "1.0.10"
 __all__ = [
     "BaseAgent",
     "BasePlugin",
